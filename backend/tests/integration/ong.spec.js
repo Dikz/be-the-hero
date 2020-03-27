b41c6d07047f53af4ba929a198f86582d6436630
Dikz <cicerogabriel12@hotmail.com>
2020-03-27 16:05:45 -0300

    Validations + Tests

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const app = require("../../src/app");
+const connection = require("../../src/database/connection");
+
+describe("ONG", () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it("should be able to create a new ONG", async () => {
+    const response = await request(app)
+      .post("/ongs")
+      .send({
+        name: "Nova Ong",
+        email: "contato@ong.com",
+        whatsapp: "99980706050",
+        city: "São Paulo",
+        uf: "SP"
+      });
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.id).toHaveLength(8);
+  });
+});
